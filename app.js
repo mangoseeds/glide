d380ee3aa4aa3ccf5fdae3179ac2d883e9deea66
mangoseeds <juyonbae@gmail.com>
2023-09-26 17:38:10 +0900

    input box and button added

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-// function initMap(){
-
-//     var options = {
-//         center: {lat: 37.5617275, lng: 126.9467413},
-//         zoom: 16
-//     }
-
-//     map = new google.maps.Map(document.getElementById("map"), options)
-
-// }
-
+// Initialize variables for the map and directions service
 let map;
+let directionsService;
+let directionsDisplay;
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
@@ -18,6 +10,50 @@ async function initMap() {
     center: { lat: 37.561, lng: 126.946 },
     zoom: 16,
   });
+
+  
+  // Initialize the directions service and display
+  directionsService = new google.maps.DirectionsService();
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsDisplay.setMap(map);
+
+  // Handle button click event
+  document.getElementById('calculateRoute').addEventListener('click', calculateRoute);
+
+
+//   google.maps.event.addListener(map, "click", (event) => {
+//     addMarker({location: event.latLng});
+//   })
+
 }
 
-initMap();
+function calculateRoute() {
+    // Get origin and destination from input fields
+    const origin = document.getElementById('origin').value;
+    const destination = document.getElementById('destination').value;
+
+    // Check if both origin and destination are provided
+    if (origin === '' || destination === '') {
+        alert('Please enter both origin and destination.');
+        return;
+    }
+
+    // Create a directions request
+    const request = {
+        origin: origin,
+        destination: destination,
+        travelMode: 'DRIVING' // You can change the travel mode as needed
+    };
+
+    // Calculate and display the route
+    directionsService.route(request, function (result, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(result);
+        } else {
+            alert('Error calculating route: ' + status);
+        }
+    });
+}
+
+// Initialize the map when the page loads
+google.maps.event.addDomListener(window, 'load', initMap);
