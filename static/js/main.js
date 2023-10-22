d4d11e5f689526cd74c2ebb368569c5595d65647
mangoseeds <juyonbae@gmail.com>
2023-10-22 13:44:34 +0900

    structure organized

--- static/js/main.js
+++ static/js/main.js
@@ -1,63 +1,40 @@
-// Initialize variables for the map and directions service
+const buildingsURL = "../glide/static/data/buildings.json" ; // Replace with the path to your JSON file
+const originInput = document.getElementById("origin");
+const destinationInput = document.getElementById("destination");
+const originBuildingsListWrapper = document.getElementById("origin-buildings-list-wrapper");
+const originBuildingsList = document.getElementById("origin-buildings-list");
+const destinationBuildingsListWrapper = document.getElementById("destination-buildings-list-wrapper");
+const destinationBuildingsList = document.getElementById("destination-buildings-list");
+const swapButton = document.getElementById("swap-button");
+
 let map;
 let directionsService;
 let directionsDisplay;
-
 let buildingsData;
 
-const buildingsURL = "/static/data/buildings.json" ; // Replace with the path to your JSON file
+let defaultMapLatLng = { lat: 37.563434, lng: 126.947945 }; //overall school scope
+let mapOptions;
 
-async function initMap() {
+function initMap() {
+  mapOptions = {
+    center: defaultMapLatLng,
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
 
-  const { Map } = await google.maps.importLibrary("maps");
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: 37.561, lng: 126.946 },
-    zoom: 16,
-  });
+  // create a DirectionsService object to use the route method and get a result for the request
+  directionsService = new google.maps.DirectionsService();
+  // create a DirectionsRenderer object which will be used to display the routes
+  directionsDisplay = new google.maps.DirectionsRenderer();
 
-  // Initialize the directions service and display
-  // directionsService = new google.maps.DirectionsService();
-  // directionsDisplay = new google.maps.DirectionsRenderer();
-  // directionsDisplay.setMap(map);
+  directionsDisplay.setMap(map);
 
-  // Handle button click event
-  document.getElementById("findroute-button").addEventListener('click', calculateRoute);
-
-}
-
-function calculateRoute() {
-  // Get origin and destination from input fields
-  const origin = document.getElementById("origin").value;
-  const destination = document.getElementById("destination").value;
-
-  // Check if both origin and destination are provided
-  if (origin === '' && destination === '') {
-    alert('입력이 올바른지 확인해주세요.');
-    return;
-  } else if (!buildingsData.contains(origin)) {
-    alert('출발지가 올바른지 확인해주세요.');
-    return;
-  } else if (!buildingsData.contains(destination)) {
-    alert('도착지가 올바른지 확인해주세요.');
-    return;
-  } else if (buildingsData.includes(origin) && buildingsData.includes(destination)) {
-    print("yes!")
-    //find route algo
-  } else {
-    alert('입력이 올바른지 확인해주세요.');
-    return;
-  }
+  // // Handle button click event
+  // document.getElementById('findroute-button').addEventListener('click', calculateRoute);
 
 }
-
-const originInput = document.getElementById("origin");
-const originBuildingsListWrapper = document.getElementById("origin-buildings-list-wrapper");
-const originBuildingsList = document.getElementById("origin-buildings-list");
-
-const destinationInput = document.getElementById("destination");
-const destinationBuildingsListWrapper = document.getElementById("destination-buildings-list-wrapper");
-const destinationBuildingsList = document.getElementById("destination-buildings-list");
 
 function autocomplete(input, list, wrapper) {
   input.addEventListener('input', function () {
@@ -84,7 +61,7 @@ function autocomplete(input, list, wrapper) {
         const suggestion = document.createElement('div');
         suggestion.innerHTML = list[i];
 
-        suggestion.classList.add('buildings-list'); 
+        suggestion.classList.add('buildings-list');
 
         suggestion.addEventListener('click', function () {
           input.value = this.innerHTML;
@@ -105,7 +82,9 @@ function autocomplete(input, list, wrapper) {
   }
 }
 
-fetch(buildingsURL)
+document.addEventListener("DOMContentLoaded", function() {
+
+  fetch(buildingsURL)
   .then(response => response.json())
   .then(buildings => {
     buildingsData = buildings; // Store the building data
@@ -117,48 +96,106 @@ fetch(buildingsURL)
 
   })
   .catch(error => console.error('Error fetching JSON data:', error));
+  
+  // Add a click event listener to the swap button
+  swapButton.addEventListener("click", function() {
+    // Get the current values of origin and destination inputs
+    const originValue = originInput.value;
+    const destinationValue = destinationInput.value;
 
+    // Check if both inputs are empty
+    if (!originValue && !destinationValue) {
+      return;
+    }
 
-  document.addEventListener("DOMContentLoaded", function() {
-    // Get references to the origin and destination input elements
-    const originInput = document.getElementById("origin");
-    const destinationInput = document.getElementById("destination");
-  
-    // Get a reference to the swap button
-    const swapButton = document.getElementById("swap-button");
-  
-    // Add a click event listener to the swap button
-    swapButton.addEventListener("click", function() {
-      // Get the current values of origin and destination inputs
-      const originValue = originInput.value;
-      const destinationValue = destinationInput.value;
-  
-      // Check if both inputs are empty
-      if (!originValue && !destinationValue) {
-        // If both inputs are empty, do nothing
-        return;
-      }
-      
-      // Add a smooth transition effect to the text values
-      originInput.style.transition = "color 0.5s";
-      destinationInput.style.transition = "color 0.5s";
-  
-      // Temporarily change the text color to transparent
-      originInput.style.color = "transparent";
-      destinationInput.style.color = "transparent";
-  
-      // After a short delay, swap the values and fade them back in
-      setTimeout(() => {
-        originInput.value = destinationValue;
-        destinationInput.value = originValue;
-  
-        // Restore text color
-        originInput.style.color = "var(--green)";
-        destinationInput.style.color = "var(--green)";
-  
-        // Clear the transition property to avoid affecting future input changes
-        originInput.style.transition = "color 0.6s";
-        destinationInput.style.transition = "color 0.6s";
-      }, 250); // delay milliseconds
-    });
+    // Add transition effect to the text values
+    originInput.style.transition = "color 0.5s";
+    destinationInput.style.transition = "color 0.5s";
+
+    // Temporarily change the text color to transparent
+    originInput.style.color = "transparent";
+    destinationInput.style.color = "transparent";
+
+    // After a short delay, swap the values and fade them back in
+    setTimeout(() => {
+      originInput.value = destinationValue;
+      destinationInput.value = originValue;
+
+      // Restore text color
+      originInput.style.color = "var(--green)";
+      destinationInput.style.color = "var(--green)";
+
+      // Clear the transition property to avoid affecting future input changes
+      originInput.style.transition = "color 0.6s";
+      destinationInput.style.transition = "color 0.6s";
+    }, 250); // delay milliseconds
   });
+});
+
+
+function calculateRoute() {
+
+  const origin = originInput.value;
+  const destination = destinationInput.value;
+
+  // Verify if the input values are valid building names
+  if (!isValidBuildingName(origin) || !isValidBuildingName(destination)) {
+    // Display an error message if input is invalid
+    const output = document.querySelector('#output');
+    output.innerHTML = "<div class='alert-danger'>Please enter valid building names.</div>";
+    return;
+  }
+
+  // create request
+  const request = {
+    origin: origin,
+    destination: destination,
+    travelMode: google.maps.TravelMode.WALKING, // available modes: WALKING, DRIVING, BICYCLING, TRANSIT
+    unitSystem: google.maps.UnitSystem.METRIC
+  }
+
+  // pass the request to the route method
+  directionsService.route(request, (result, status) => {
+    if (status === google.maps.DirectionsStatus.OK) {
+      // get distance and time
+      displayRoute(result);
+    } else {
+      // Display error message
+      handleRouteError();
+    }
+  });
+}
+
+function isValidBuildingName(buildingName) {
+  return buildingsData.includes(buildingName);
+}
+
+function displayRoute(result) {
+  // Display route information on the map
+  const output = document.querySelector('#output');
+  output.innerHTML = "<div class='alert-info'> From: " + originInput.value
+      + " .<br />To: " + destinationInput.value
+      + " .<br />Walking Distance: " + result.routes[0].legs[0].distance.text
+      + " .<br />Duration: " + result.routes[0].legs[0].duration.text
+      + " .</div>";
+
+  // Set the map to display the route
+  directionsDisplay.setDirections(result);
+}
+
+function handleRouteError(){
+  // Display error message
+  const output = document.querySelector('#output');
+  output.innerHTML = "<div class='alert-danger'>Could not retrieve walking route.</div>";
+  
+  // Delete route from map
+  directionsDisplay.setDirections({ routes: [] });
+
+  // Center map back to default
+  map.setCenter(defaultMapLatLng);
+}
+
+document.getElementById('findroute-button').addEventListener('click', () => {
+  calculateRoute(); // Call the route calculation function
+});
+
