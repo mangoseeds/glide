3b00253807df01dfa651ef9650642f561467ddb2
mangoseeds <juyonbae@gmail.com>
2023-10-22 16:03:32 +0900

    firebase config inserted to __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,20 @@
 from flask import Flask
+import pyrebase
+
+config = {
+        "apiKey": "AIzaSyCrpjD8HRk2YFPRWu0YB78By_OH3fyySQg",
+        "authDomain": "glide-cce9b.firebaseapp.com",
+        "databaseURL": "https://glide-cce9b-default-rtdb.firebaseio.com",
+        "projectId": "glide-cce9b",
+        "storageBucket": "glide-cce9b.appspot.com",
+        "messagingSenderId": "959009446750",
+        "appId": "1:959009446750:web:1ed6c30c71b082b6cacdcf"
+}
 
 app = Flask(__name__)
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
 
 # Configuration settings and any app-wide setup can go here
 
